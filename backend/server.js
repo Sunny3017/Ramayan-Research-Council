9f00dae7fab14f4b763744d6c929395950187bb7
Sunny3017 <sunnychaudhary3792@gmail.com>
2026-01-29 20:19:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -81,6 +81,7 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+    console.log("Developed by Sunny Chaudhary || KASHI DIGITAL LABS || makha laadle meowwwwwwwwww");
 });
 
 // Handle unhandled promise rejections
